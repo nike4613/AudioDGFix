90900e1cefc89815e6226ea6cfd690d6834c4c48
Anairkoen Schno <aaron.cirr.com@gmail.com>
2021-03-28 23:11:26 -0500

    Add project files.

---
+++ AudioDGFix/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+#if DEBUG
+//Console.ReadLine();
+Debugger.Launch();
+#endif
+
+Console.WriteLine("Searching for audiodg.exe");
+
+var processes = Process.GetProcessesByName("audiodg");
+
+foreach (var process in processes)
+{
+    Console.WriteLine($"Found {process.ProcessName} ({process.Id})");
+
+    // set high priority
+    process.PriorityClass = ProcessPriorityClass.High;
+    process.ProcessorAffinity = (IntPtr)0x00000001;
+
+    Console.WriteLine($"Priority {process.PriorityClass}, affinity {process.ProcessorAffinity}");
+}
