676afe2d776fcb244a966d9d77e0b732b9fcec85
Anairkoen Schno <aaron.cirr.com@gmail.com>
2021-03-30 17:31:22 -0500

    Adjust service install script to require PS Core

--- install-service.ps1
+++ install-service.ps1
@@ -1,14 +1,19 @@
+if ($PSEdition -neq "Core") {
+    Write-Host "This script requires PowerShell Core. Get it here: https://github.com/powershell/powershell#get-powershell"
+    $HOST.UI.RawUI.ReadKey(“NoEcho,IncludeKeyDown”) | OUT-NULL
+    $HOST.UI.RawUI.Flushinputbuffer()
+}
 
 # Ensure that  the script is elevated
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
   # Relaunch as an elevated process:
-  Start-Process powershell "-ExecutionPolicy","Unrestricted","-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  Start-Process pwsh "-ExecutionPolicy","Unrestricted","-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
   exit
 }
 
 Set-Location -LiteralPath $PSScriptRoot
 # Create the service 
-New-Service -Name "AudioDGFix" -BinaryPathName "`"$(resolve-path .\AudioDGFix.exe)`" -s" -DependsOn "audiosrv" -StartupType Automatic
+New-Service -Name "AudioDGFix" -BinaryPathName "`"$(resolve-path .\AudioDGFix.exe)`" -s" -DependsOn "audiosrv" -StartupType AutomaticDelayedStart
 # Immediately start the Service
 Start-Service -Name "AudioDGFix"
--- uninstall-service.ps1
+++ uninstall-service.ps1
@@ -1,4 +1,8 @@
-#Requires -Version 6.0
+if ($PSEdition -neq "Core") {
+    Write-Host "This script requires PowerShell Core. Get it here: https://github.com/powershell/powershell#get-powershell"
+    $HOST.UI.RawUI.ReadKey(“NoEcho,IncludeKeyDown”) | OUT-NULL
+    $HOST.UI.RawUI.Flushinputbuffer()
+}
 
 # Ensure that  the script is elevated
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
